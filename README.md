74b89dd040ec2889b02c5b67cc00c23128957bb2
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-03-29 16:05:45 +0530

    Updated the read me file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # chatGPTFromTerminal
+
+Use chatGPT from your terminal without ever opening your browser.
+# HOW TO USE
+  - Copy the .jar and .env files located in ***out/artifacts/chatGPTFromTerminal_jar*** and
+    ***root*** respectively.
+  - Paste your api key in .env file.
+    * You can find your api key [here](https://platform.openai.com/account/api-keys).
+  - Copy ***both*** files ***( .jar and .env )*** to the root directory ( like C:\ ).
+  - Make an alias for running the .jar file.
+    * Goto ***Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor*** .
+    * Right-click on the pane, select new > string value and name it ***AutoRun***.
+    * Right-click on the AutoRun property and select Modify in the Value Data section enter -
+    *     doskey <alias_name> = java -jar <full directory where the .jar file is loacted>
+    *     doskey a = java -jar C:\chatGPTFromTerminal.jar
+  - Now try to open a new instance of terminal and run your alias.
+
--- src/main/java/com/suprhulk/Main.java
+++ src/main/java/com/suprhulk/Main.java
@@ -1,5 +1,43 @@
-package com.suprhulk;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package com.suprhulk;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+
+        Dotenv config;
+
+        System.out.print("Enter your query: ");
+        String query = sc.nextLine();
+        query = query +" in less than 2000 tokens";
+        String temp = "CompletionChoice(text="+query;
+        OpenAiService service = new OpenAiService(Dotenv.configure().load().get("TOKEN"));
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt(query)
+                .model("text-davinci-003")
+                .maxTokens(2000)
+                .echo(true)
+                .build();
+
+        try {
+            String s = String.valueOf(service.createCompletion(completionRequest).getChoices().get(0));
+            int i=0;
+            for(; i<temp.length(); i++) {
+                if(s.charAt(i)!=temp.charAt(i)) {
+                    break;
+                }
+            }
+            System.out.println(s.substring(i, s.indexOf("index=0")));
+        }
+        catch (Exception e) {
+            System.err.println("Failed to generate response: " + e.getMessage());
+        }
     }
 }
